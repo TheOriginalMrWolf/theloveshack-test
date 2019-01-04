437b960e304856c3ada0284929ec62148d4fc156
Armin Aschoff <a.aschoff@gmail.com>
2019-01-04 17:49:33 +1100

    hmmm

--- weather-worker.js
+++ weather-worker.js
@@ -1,12 +1,14 @@
 
-/*var req = new XMLHttpRequest();  
-req.open('GET', 'http://www.mydomain.com/', false);   
-req.send(null);  
-if(req.status == 200)  
-   dump(req.responseText);
+/*
    */
 
 onmessage = function(msg) {
     console.log(msg);
-    postMessage("Received message: " + msg);
+    
+    var req = new XMLHttpRequest(); 
+    req.open('GET', msg.data, false);   
+    req.send(null);  
+
+    if(req.status == 200)  
+       postMessage(req.responseText);
 }
