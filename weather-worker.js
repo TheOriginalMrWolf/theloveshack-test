f25c1ef53a0922e40c6bebbe2b260e123e026f37
Armin Aschoff <a.aschoff@gmail.com>
2019-01-04 17:36:55 +1100

    web worker

---
+++ weather-worker.js
@@ -0,0 +1,9 @@
+
+/*var req = new XMLHttpRequest();  
+req.open('GET', 'http://www.mydomain.com/', false);   
+req.send(null);  
+if(req.status == 200)  
+   dump(req.responseText);
+   */
+
+postMessage("Hi There!");
