d7bbe775a3f2aafb846222df722ceafb5d42f567
Armin Aschoff <a.aschoff@gmail.com>
2019-01-04 18:07:02 +1100

    who knows

--- weather-worker.js
+++ weather-worker.js
@@ -1,13 +1,13 @@
-
-/*
-   */
-
 onmessage = function(msg) {
     console.log("Inside: ", msg.data);
     
     var req = new XMLHttpRequest(); 
-    req.open('GET', msg.data, false);   
-    req.send(null);  
+    req.open('GET', msg.data, false); 
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            postMessage(req.responseText);
+        }
+    }; 
 
-    postMessage(req.responseText);
+    req.send(null);  
 }
